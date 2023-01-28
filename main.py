dd32e4e6c7b34460f917715c188ecf8e2e6e99f3
Tiane Maestas <tiane.developer@gmail.com>
2023-01-27 20:59:47 -0800

    Extra params added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from orbits import TwoBodyKeplerOrbit
-import numpy
 
-test_position = numpy.array([12426, 0, 0])
-test_velocity = numpy.array([0, 4.78, 0])
+# test_position = [12426, 0, 0]
+# test_velocity = [0, 4.78, 0]
+# test_orbit = TwoBodyKeplerOrbit(test_position, test_velocity)
+# print(test_orbit)
 
-test_orbit = TwoBodyKeplerOrbit(test_position, test_velocity)
-
+test_orbit = TwoBodyKeplerOrbit.build_from_known_params(9649.6, 0.2877, 180)
 print(test_orbit)
--- orbits.py
+++ orbits.py
@@ -29,17 +29,41 @@ class TwoBodyKeplerOrbit:
     EARTH_MU = 3.986e5  # km^3/s^2
 
     def __init__(self, position_vector, velocity_vector) -> None:
+        """Initialize from a measured position and velocity."""
         # only in radial direction (r, 0, 0)
-        self.position_vector = position_vector
+        self.position_vector = numpy.array(position_vector)
         # only in radial and theta (r, theta, 0)
-        self.velocity_vector = velocity_vector
+        self.velocity_vector = numpy.array(velocity_vector)
 
         self.calculate_orbit_info()
+    
+    @classmethod
+    def build_from_known_params(cls, semi_major_axis, eccentricity, inclination_angle, ascending_node, argument_of_periapses, initial_mean_anomaly) -> None:
+        """Initialize from known orbital parameters."""
+        # Note: maybe add angle type so they can pass in deg or rad.
+    
+    @classmethod
+    def build_from_known_params(cls, semi_major_axis, eccentricity, true_anomaly) -> None:
+        """Initialize from known orbital parameters."""
+        # Note: maybe add angle type so they can pass in deg or rad.
+        true_anomaly = numpy.radians(true_anomaly)
+        H = numpy.sqrt(TwoBodyKeplerOrbit.EARTH_MU * semi_major_axis * (1 - numpy.power(eccentricity, 2)))
+        
+        # Calculate position vector.
+        r = semi_major_axis * (1 - numpy.power(eccentricity, 2)) / (1 + eccentricity * numpy.cos(true_anomaly))
+        position_vector = [r, 0, 0]
+
+        # Calculate velocity vector.
+        v_r = (eccentricity * TwoBodyKeplerOrbit.EARTH_MU / H) * numpy.sin(true_anomaly)
+        v_theta = (TwoBodyKeplerOrbit.EARTH_MU / H) * (1 + eccentricity * numpy.cos(true_anomaly))
+        velocity_vector = [v_r, v_theta, 0]
+
+        return cls(position_vector, velocity_vector)
 
     def update(self, radial_vector, velocity_vector) -> None:
         """Not sure about this yet."""
 
-    def calculate_orbit_info(self):
+    def calculate_orbit_info(self) -> None:
         """This calculates all of the info that can be displayed via this 'to string' method."""
         # using this weird extra definition because numpy is bugged and it says code isn't reachable otherwise.
         def cross(x, y): return numpy.cross(x, y)
@@ -58,15 +82,20 @@ class TwoBodyKeplerOrbit:
 
         self.semi_minor_axis = self.semi_major_axis * numpy.sqrt(1 - self.eccentricity**2)
 
+        self.period = 2 * numpy.pi * numpy.sqrt(numpy.power(self.semi_major_axis, 3)/TwoBodyKeplerOrbit.EARTH_MU)
+
         # Closest point
         self.perigee = self.semi_major_axis * (1 - self.eccentricity)
 
         # Furthest point
         self.apogee = self.semi_major_axis * (1 + self.eccentricity)
 
-        self.true_anomaly = numpy.degrees(numpy.arccos(1 / self.eccentricity * (self.parameter / numpy.linalg.norm(self.position_vector) - 1)))
+        argument = numpy.clip(1 / self.eccentricity * (self.parameter / numpy.linalg.norm(self.position_vector) - 1), -1, 1) # in case of rounding errors.
+        self.true_anomaly = numpy.degrees(numpy.arccos(argument))
 
-    ORBIT_INFO_FORMAT = "\n-----Orbit INFO-----\nOrbit Type: {orbit_type}\nPosition: {position}\nVelocity: {velocity}\nAngular Momentum(H): {angular_momentum}\nTotal Energy(E): {total_energy}\nSemi-Major Axis(a): {semi_major_axis}\nSemi-Minor Axis(b): {semi_minor_axis}\nParameter(p): {parameter}\nEccentricity(e): {eccentricity}\nPerigee: {perigee}\nApogee: {apogee}\nTrue Anomaly: {true_anomaly}\n"
+        self.flight_path_angle = numpy.degrees(numpy.arctan(self.eccentricity * numpy.sin(self.true_anomaly) / (1 + self.eccentricity * numpy.cos(self.true_anomaly))))
+
+    ORBIT_INFO_FORMAT = "\n-----Orbit INFO-----\nOrbit Type: {orbit_type}\nPosition: {position}\nVelocity: {velocity}\nAngular Momentum(H): {angular_momentum}\nTotal Energy(E): {total_energy}\nSemi-Major Axis(a): {semi_major_axis}\nSemi-Minor Axis(b): {semi_minor_axis}\nParameter(p): {parameter}\nEccentricity(e): {eccentricity}\nPeriod(T): {period}\nPerigee: {perigee}\nApogee: {apogee}\nTrue Anomaly(f): {true_anomaly}\nFlight Path Angle(gamma): {flight_path_angle}\n"
     def __str__(self) -> str:
         return TwoBodyKeplerOrbit.ORBIT_INFO_FORMAT.format(orbit_type=self.orbit_type,
                                                            position=self.position_vector,
@@ -77,6 +106,8 @@ class TwoBodyKeplerOrbit:
                                                            semi_minor_axis=self.semi_minor_axis,
                                                            parameter=self.parameter,
                                                            eccentricity=self.eccentricity,
+                                                           period=self.period,
                                                            perigee=self.perigee,
                                                            apogee=self.apogee,
-                                                           true_anomaly=self.true_anomaly)
+                                                           true_anomaly=self.true_anomaly,
+                                                           flight_path_angle=self.flight_path_angle)
